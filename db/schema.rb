849f3cbfdaaae38c88f3aeb1472530262e5d89f1
Chris Hughes <chris-git@fysh.org>
2015-08-23 19:10:31 +0100

    Add Property

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150823180716) do
+ActiveRecord::Schema.define(version: 20150823180917) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 20150823180716) do
     t.date     "available_until"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+  end
+
+  create_table "properties", force: :cascade do |t|
+    t.string   "name"
+    t.string   "presentation"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
 end
